e14b11462462fb6890a328430dad6bf93ea4558e
Leman-Mustafazadeh <lmanmustafazad6@gmail.com>
2023-03-31 13:39:48 +0400

    name

---
+++ exam.js
@@ -0,0 +1,76 @@
+//    1ci
+    // for(i=0;i<100;i++){
+        
+    //     if(i=0 && i<100){
+    //         console.log("sayi 0 ile 100 arasinda");
+    //     }else{
+    //         console.log("sayi 0 ile 100 arasinda degil");
+    //     }
+    // }   
+
+
+
+    // for(i=0;i<)
+
+    // //   2ci
+    // if(i%2==0){
+    //     console.log("cift");
+    // }else{
+    //     console.log("tek");
+    // }
+
+
+   
+
+    // //    3cu
+    // readline.question('How old are you? What is your name?', yash => {
+    //     if(yash>18){
+    //         console.log("Bu siteye erisim iznini var")
+    //     }else if(yash==18){
+    //         console.log("18e beraberdi");
+    //     }
+    //     else{
+    //         console.log("Bu siteye erisim izniniz yok")
+    //     }
+    //     readline.close()
+    // });
+
+
+
+    // //5ci
+    //  for(i=0;i<100;i++){
+    //     if(90<i<100){
+    //         console.log("AA");
+    //     }else if(80<i<89){
+    //         console.log("BA");
+    //     }else if(70<i<79){
+    //         console.log("BB");
+    //     }else if(60<i<69){
+    //         console.log("CB");
+    //     }else if(50<i<59){
+    //         console.log("CC");
+    //     }else if(40<i<49){
+    //         console.log("DC");
+    //     }else if(30<i<39){
+    //         console.log("DD");
+    //     }else if(0<i<29){
+    //         console.log("FF");
+    //     }
+    //  }
+
+
+    
+
+
+
+
+    // 8 ci
+    // let word = "Leman";
+    // let surName = "mustafazade";
+    // let fullName = word.concat(surName)
+
+    // if(word='Jack',surName = "Smith"){
+    //     console.log("Merhaba,Jack Smith")
+    // }else{
+    //     console.log("Merhaba,[word] [surName]!");
+    // }
